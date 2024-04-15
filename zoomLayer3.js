7ad9a2cdcfec9097de8a73f5f40668f84423c52c
Michelle Lin <104107202+lin-shu-yu@users.noreply.github.com>
2024-04-15 16:30:42 -0400

    Update zoomLayer3.js
    testing description for selection

--- zoomLayer3.js
+++ zoomLayer3.js
@@ -14,7 +14,9 @@ Define content for each box, CHANGE EXPLANATION TEXTS HERE*/
 var boxContents = {
     selection: {
         title: "Selection",
-        explanation: "Explanation for Selection."
+        explanation: "Astronaut selection was originally carried out by NASA and the Soviet Space Program, and now Japan, China, Russia, Brazil, and countries in Europe have selected their own astronauts [NASA 2024]. Due to historical and sociopolitical factors, only male candidates were originally allowed to become astronauts, with one exception in the early 1960s (Valentina Tereshkova, USSR). Female candidates were accepted regularly in the 1980s, although still comprise a minority of the program [Steimle and Norberg 2013]."
+
+"Selection criteria include educational/professional qualifications and candidates must be able to pass the astronaut physical, medical, and psychological exams. More restrictively, astronaut candidates from most programs must exhibit mental and personal capabilities which are selected for and evaluated by a panel of experts [Steimle and Norberg 2013]."
     },
     loss_of_mission: {
         title: "Loss of Mission",
